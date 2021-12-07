1c628cd2a09f62b35458dbd8f21a57f756e47917
and028 <91878390+and028@users.noreply.github.com>
2021-12-07 17:01:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,13 @@
 #include "lib.h"
 using namespace std;
 int main() {
-
     int dim=0;
-    float vettore[dim];
-
     cin>>dim;
-    for (int i=0; i<dim; i++){
-        cin>>vettore[i];
+    float vettore[dim];
+    for(int i=0; i<dim; i++){
+        cin >> vettore[i];
     }
-    float massimo= max (vettore,dim);
-    cout<<massimo<<endl;
+    cout<<max(vettore,dim)<<endl;
+
     return 0;
 }
