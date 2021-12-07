318921b0e6c24d02629307b06167daa8ac8ae8d5
and028 <91878390+and028@users.noreply.github.com>
2021-12-07 17:00:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,8 @@
 #include "lib.h"
-float max (float vettore[],int dim){
-
-    float massimo=0;
-    for (int i=0; i<dim; i++){
-        if(vettore[i]>vettore[i-1]){
+float max(float vettore[],int dim){
+    float massimo=vettore[0];
+    for (int i=0;i<dim;i++){
+        if (vettore[i]>massimo){
             massimo=vettore[i];
         }
     }
